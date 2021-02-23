ec90c99384e069b9459f362f87e88438a1477707
Cattani <contato.cattani@gmail.com>
2021-02-23 14:45:52 -0300

    add css for makeup elixir

--- lib/notes/blog/blog.ex
+++ lib/notes/blog/blog.ex
@@ -7,16 +7,15 @@ defmodule Notes.Blog do
     as: :posts,
     highlighters: [:makeup_elixir, :makeup_erlang]
 
-  # The @posts variable is first defined by NimblePublisher.
-  # Let's further modify it by sorting all posts by descending date.
   @posts Enum.sort_by(@posts, & &1.date, {:desc, Date})
 
-  # Let's also get all tags
   @tags @posts |> Enum.flat_map(& &1.tags) |> Enum.uniq() |> Enum.sort()
 
-  # And finally export them
   def all_posts, do: @posts
   def all_tags, do: @tags
+
+  def published_posts, do: Enum.filter(all_posts(), &(&1.published == true))
+  def recent_posts(num \\ 5), do: Enum.take(published_posts(), num)
 
   defmodule NotFoundError, do: defexception [:message, plug_status: 404]
 
